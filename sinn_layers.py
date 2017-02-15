Fixed softmax error
Tweaked model architecture as the softmax was previously causing
issues. Added distributed initiation option.

sinn_layers.py
@@ -10,7 +10,7 @@ class Metric(Layer):
         self.nodes     = len(initiation_matrix)
 
         self.B = K.variable(initiation_matrix)
-        self.P = K.variable(2)
+        self.P = K.variable(4)
 
         # keras variables
         self.inbound_nodes = []
@@ -29,7 +29,7 @@ class Metric(Layer):
         self.A = K.variable(np.full((self.nodes, self.input_dim), 1,dtype='float64'))
 
         # tell keras which ones are trained
-        self.trainable_weights = [self.W,self.B,self.A]
+        self.trainable_weights = [self.W,self.A]
 
         # keras constructor call
         super(Metric, self).build(input_shape)
@@ -50,7 +50,7 @@ class Metric(Layer):
         B = K.expand_dims(self.B, dim=0)
         B = K.repeat_elements(B,batch_size,0)
 
-        result = 1 / ( K.sum(K.abs(A * (W * x - B)), axis=2) + 1e-6)
+        result = 1 / ( K.sum(K.abs(A * (W * x - B)), axis=2) + 1e-10)
         denominator = K.max(result, axis = 1)
         denominator = K.expand_dims(denominator,dim=1)
         denominator = K.repeat_elements(denominator, self.nodes, axis=1)
@@ -99,7 +99,7 @@ class Shepard(Layer):
         y = K.expand_dims(y,dim=1)
         y = K.repeat_elements(y,input_size,1)
 
-        x = x / ( y + 1e-6)
+        x = x / ( y + 1e-10)
 
         x = K.expand_dims(x, dim=1)
         x = K.repeat_elements(x, self.output_dim, 1)

sinn_model.py
@@ -1,13 +1,13 @@
 # SINN model with k-means initiation
 from sklearn.cluster import KMeans
 import numpy as np
-from keras.layers import Input
+from keras.layers import Input, Activation
 from keras.models import Model
 from sinn_layers import Metric, Shepard
 
 class SINN_model:
 
-	def __init__(self, input_matrix, output_matrix, num_of_clusters, nodes_per_cluster, batch_size, epochs, verbose=2, validation_split=0, shuffle_data=True, model_optimizer='RMSprop', loss_metric='mse'):
+	def __init__(self, input_matrix, output_matrix, num_of_clusters, nodes_per_cluster, batch_size, epochs, mode='classification', verbose=2, validation_split=0.05, shuffle_data=True, model_optimizer='RMSprop', loss_metric='mse', initiation_mode='k_means'):
 
 		# keep training parameters
 		self.batch_size 		= batch_size
@@ -16,15 +16,85 @@ class SINN_model:
 		self.validation_split 	= validation_split
 		self.shuffle_data		= shuffle_data
 
-		# build k-means clusters
-		kmeans  = KMeans(n_clusters=num_of_clusters, random_state=0).fit(input_matrix)
 
+		(separated_data,separated_labels) = self.separate_data_by_class(input_matrix,output_matrix)
+
+		( initiation_data , initiation_labels ) = ([],[])
+
+		if( initiation_mode == 'k_means'):
+			(initiation_data , initiation_labels) = self.k_means_initiation(separated_data,separated_labels,num_of_clusters,nodes_per_cluster)
+		elif (initiation_mode == 'distributed'):
+			(initiation_data,initiation_labels) = self.distributed_initiation(separated_data,separated_labels,num_of_clusters*nodes_per_cluster)
+
+		print('number of encoded nodes : ' + str(len(initiation_data)))
+
+		# build model from diverse dataset picked using k-means
+		input_size = len(input_matrix[0])
+		input_layer = Input(shape=(input_size,))
+
+		# metric layer
+		m = Metric(initiation_data)(input_layer)
+
+		# shepard layer
+		s = Shepard(initiation_labels)(m)
+
+		if(mode == 'classification'):
+
+			# softmax = Dense(len(output_matrix[0]), activation='softmax')(s)
+			softmax = Activation('softmax')(s)
+			self.model = Model(input=input_layer, output=softmax)
+			self.model.compile(optimizer=model_optimizer,loss=loss_metric,metrics=['categorical_accuracy'])
+
+		else:
+
+			self.model = Model(input=input_layer, output=s)
+			self.model.compile(optimizer=model_optimizer,loss=loss_metric)
+
+		print('model complete...')
+
+	def k_means_initiation(self,separated_data,separated_labels,num_of_clusters,nodes_per_cluster):
+
+		initiation_data   = []
+		initiation_labels = []
+		
+		for i in range(len(separated_data)):
+
+			(metric_initiation,shepard_initiation) = self.get_clusters(num_of_clusters,nodes_per_cluster,separated_data[i],separated_labels[i])
+
+			initiation_data   += metric_initiation
+			initiation_labels += shepard_initiation
+
+		return (initiation_data,initiation_labels)
+
+
+	def distributed_initiation(self,separated_data,separated_labels,nodes_per_class):
+
+		initiation_data   = []
+		initiation_labels = []
+
+		for i in range(len(separated_data)):
+
+			for j in range( min( nodes_per_class , len(separated_data[i]) )):
+
+				initiation_data.append(separated_data[i][j])
+				initiation_labels.append(separated_labels[i][j])
+
+		return (initiation_data,initiation_labels)
+
+
+	def get_clusters(self,num_of_clusters,nodes_per_cluster,input_matrix,output_matrix):
+		# build k-means clusters
+		print('building k-means clusters...')
+		kmeans  = KMeans(n_clusters=num_of_clusters, random_state=0, max_iter=1000,n_init=20).fit(input_matrix)
+		print('done...')
 		centers = kmeans.cluster_centers_
 		labels  = kmeans.labels_
 
 		# seperate labelled data by cluster, a maximum of nodes_per_cluster data points are kept for each cluster
 		cluster_nodes = []
 		cluster_topology = []
+
+		print('separating clusters...')
 
 		for i in range(num_of_clusters):
 			cluster_nodes.append([])
@@ -41,6 +111,8 @@ class SINN_model:
 			if(self.is_matrix_valid(nodes_per_cluster,cluster_nodes)):
 				break
 
+		print('done...')
+		print('building initiation matrices...')
 		metric_initiation  = []
 		shepard_initiation = []
 
@@ -49,31 +121,33 @@ class SINN_model:
 				metric_initiation.append(cluster_nodes[i][j])
 				shepard_initiation.append(cluster_topology[i][j])
 
-		print("----------------")
-		print(metric_initiation)
-		print("----------------")
-		print(shepard_initiation)
-		print("----------------")
+		return (metric_initiation,shepard_initiation)
 
-		# build model from diverse dataset picked using k-means
-		input_size = len(input_matrix[0])
-		input_layer = Input(shape=(input_size,))
+	def separate_data_by_class(self, input_matrix, output_matrix):
 
-		# metric layer
-		m = Metric(metric_initiation)(input_layer)
+		separated_data   = []
+		separated_labels = []
 
-		# shepard layer
-		s = Shepard(shepard_initiation)(m)
+		for i in range(len(output_matrix[0])):
+			separated_data.append([])
+			separated_labels.append([])
 
-		# create model
-		self.model = Model(input=input_layer, output=s)
-		self.model.compile(optimizer=model_optimizer,loss=loss_metric)
+		for i in range(len(input_matrix)):
+
+			index = np.argmax(output_matrix[i])
+
+			separated_data[index].append(input_matrix[i])
+			separated_labels[index].append(output_matrix[i])
+
+		return (separated_data,separated_labels)
 
 	def predict(self, input_matrix):
 
 		return self.model.predict_on_batch(input_matrix)
 
 	def train_model(self, input_matrix, output_matrix):
+
+		print('training starting...')
 
 		self.model.fit( input_matrix, output_matrix , batch_size=self.batch_size, nb_epoch=self.epochs, verbose=self.verbose, validation_split=self.validation_split, shuffle=self.shuffle_data)
 

sinn_model_test.py
@@ -1,31 +1,48 @@
 # SINN_model_test.py
 
-import random
 from sinn_model import SINN_model
+from keras.datasets import mnist
+import numpy as np
 
-x_possible = [-1,1]
-y_possible = [-1,1]
-# z_possible = [-2,-1,1,2]
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-test_data   = []
-test_labels = []
+data_training   = []
+labels_training = []
 
-for i in range(100):
+data_testing   = []
+labels_testing = []
 
-	x = random.choice(x_possible)
-	y = random.choice(y_possible)
-	z = x * y
+print('starting...')
 
-	x += random.uniform(-0.1,0.1)
-	y += random.uniform(-0.1,0.1)
-	z += random.uniform(-0.1,0.1)
+for i in range(len(X_train)):
 
-	test_data.append([x,y])
-	test_labels.append([z])
+	data_training.append( X_train[i].flatten('C'))
 
-s = SINN_model(test_data,test_labels,4,4,100,10)
-s.train_model(test_data,test_labels)
+	classification = Y_train[i]
 
-test_arr = [[1,1],[-1,-1]]
+	label = np.zeros(10)
+	label[classification] = 1
 
-print(s.predict(test_arr))
+	labels_training.append(label)
+
+print('done processing training data...')
+
+for i in range(len(X_test)):
+
+	data_testing.append( X_test[i].flatten())
+
+	labels_testing.append(Y_test[i])
+
+print('done processing testing data...')
+
+data_training = np.array(data_training)
+labels_training = np.array(labels_training)
+
+data_testing = np.array(data_testing)
+labels_testing = np.array(labels_testing)
+
+s = SINN_model(data_training,labels_training,10,1,500,50,verbose=1,model_optimizer='adagrad',loss_metric='categorical_crossentropy')
+
+s.train_model(data_training,labels_training)
+
+